e66017f5a8fd548623fc0aac5bafcade146922b9
markobabic8 <markobabic056@gmail.com>
2019-11-17 19:41:25 +0100

    pravljenje prozora i prvog kvadratica

---
+++ mbtan.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <GL/glut.h>
+
+void on_display(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glColor3f(1, 0, 0);
+    glBegin(GL_POLYGON);
+        glVertex3f(0.1, -0.1, 0);
+        glVertex3f(-0.1, -0.1, 0);
+        glVertex3f(-0.1, 0.1, 0);
+        glVertex3f(0.1, 0.1, 0);
+    glEnd();
+    
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+    
+    glutDisplayFunc(on_display);
+    
+    glClearColor(0.75, 0.75, 0.75, 0);
+    
+    glutMainLoop();
+    
+    return 0;
+}
