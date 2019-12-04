21eef2b30c545f0f81d5c530856048980eb75cfd
markobabic8 <markobabic056@gmail.com>
2019-12-04 21:17:50 +0100

    Nastavak rada na projektu. Nedelja 4.

--- mbtan/main.c
+++ mbtan/main.c
@@ -6,12 +6,13 @@
 
 #define TIMER_ID 0
 #define TIMER_INTERVAL 20
-#define GRORNJA_GRANICA 0.8
+#define GORNJA_GRANICA 0.8
 #define DONJA_GRANICA -0.8
+#define MAX_BROJ_KVADRATA_U_REDU 7
 
 const static float size = 0.03;
 static int play = 0;
-static int animation_ongoing=0;
+static int animation_ongoing = 0;
 
 static float x_curr;
 static float y_curr;
@@ -20,18 +21,29 @@ static float v_y;
 static float y_pom;
 
 static int n;
-static float x_1 =-0.99;
-static float x_2 =-0.89;
-static float y_1 = 0.7;
-static float y_2 = 0.6;
+static float x_1 =-0.98;
+static float x_2 = -0.98 + (2.0 - 0.16)/7.0;
+static float y_1 = 0.76 -(2.0 - 0.16)/7.0;
+static float y_2 = 0.76 - (2.0-0.16)/7.0 - (2.0-0.16)/7.0;
 
 static int broj_kvadratica = 0;
 
 static float* niz_y;
 static float* niz_x;
+static int* postoji;
 
 void on_display(void){
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    glViewport(0,0,600,600);
+
+    glColor3f(0,0,0);
+    glBegin(GL_POLYGON);
+        glVertex2f(-1,1);
+        glVertex2f(-1,-1);
+        glVertex2f(1,-1);
+        glVertex2f(1,1);
+    glEnd();
     
     //Loptica
     if(1){
@@ -51,14 +63,14 @@ void on_display(void){
 
     glColor3f(1,1,1);
     glBegin(GL_LINES);
-        glVertex2f(-1, -0.8);
-        glVertex2f(1, -0.8);
+        glVertex2f(-1, DONJA_GRANICA);
+        glVertex2f(1, DONJA_GRANICA);
     glEnd();
 
     glColor3f(1,1,1);
     glBegin(GL_LINES);
-        glVertex2f(-1, 0.8);
-        glVertex2f(1, 0.8);
+        glVertex2f(-1, GORNJA_GRANICA);
+        glVertex2f(1, GORNJA_GRANICA);
     glEnd();
 
     if(play == 0){
@@ -71,6 +83,9 @@ void on_display(void){
     }
     else{
         glColor3f(1,1,1);
+        
+        glLineWidth(8);
+        
         glBegin(GL_LINES);
             glVertex2f(-0.96,0.96);
             glVertex2f(-0.96, 0.84);
@@ -81,31 +96,52 @@ void on_display(void){
             glVertex2f(-0.90,0.96);
             glVertex2f(-0.90, 0.84);
         glEnd();
-    }
 
-    niz_x[0] = x_1;
-    niz_x[1] = x_2;
-    niz_y[0] = y_1;
-    niz_y[1] = y_2;
-    for(int i = 0; i < 2*n; i+=2){
-        glColor3f(1, 0, 0);
-        glBegin(GL_POLYGON);
-            glVertex2f(niz_x[i],niz_y[i]);
-            glVertex2f(niz_x[i+1],niz_y[i]);
-            glVertex2f(niz_x[i+1],niz_y[i+1]);
-            glVertex2f(niz_x[i],niz_y[i+1]);
-        glEnd();
-
-        if(i+3 < 2*n){
-            niz_x[i+2] = niz_x[i] + 0.11;
-            niz_x[i+3] = niz_x[i+1] + 0.11;
-            niz_y[i+2] = niz_y[i];
-            niz_y[i+3] = niz_y[i+1];
-            printf("%f\n%f\n%f\n%f\n", niz_x[i], niz_x[i+1], niz_x[i+2], niz_x[i+3]);
-        }
+        glLineWidth(1);
     }
     
+    for(int i = 0; i < 2*n; i+=2){
+        glColor3f(1, 0, 0);
+        glLineWidth(3);
+        if(postoji[i/2] == 1){
+            glBegin(GL_LINE_LOOP);
+                glVertex2f(niz_x[i],niz_y[i]);
+                glVertex2f(niz_x[i+1],niz_y[i]);
+                glVertex2f(niz_x[i+1],niz_y[i+1]);
+                glVertex2f(niz_x[i],niz_y[i+1]);
+            glEnd();
+        }
+
+        float pomeraj = fabs(x_2 - x_1) + 0.02;
+
+        if(i+3 < 2*n){
+            niz_x[i+2] = niz_x[i] + pomeraj;
+            niz_x[i+3] = niz_x[i+1] + pomeraj;
+            niz_y[i+2] = niz_y[i];
+            niz_y[i+3] = niz_y[i+1];
+        }
+        glLineWidth(1);
+    }
+    
+    if(niz_y[n-1] <= -0.76){
+        glColor3f(1,1,1);
+        glBegin(GL_POLYGON);
+            glVertex2f(-1,1);
+            glVertex2f(-1,-1);
+            glVertex2f(1,-1);
+            glVertex2f(1,1);
+        glEnd();
+    }
+
     glutSwapBuffers();
+}
+
+static void on_timer4(int value){
+    for(int i =0; i<2*n; i++){
+        niz_y[i] -= (2.0 - 0.16)/7.0 + 0.02;
+    }
+
+    glutPostRedisplay();
 }
 
 static void on_timer(int value){
@@ -125,6 +161,7 @@ static void on_timer(int value){
     if(y_curr < -0.76){
         animation_ongoing = 0;
         play = 0;
+        glutTimerFunc(TIMER_INTERVAL, on_timer4, TIMER_ID);
     }
 
     if(animation_ongoing)
@@ -191,10 +228,26 @@ int main(int argc, char** argv){
 
     srand(time(NULL));
 
-    n = abs(ceil(rand()%3)) + 1;
+    n = abs(ceil(rand()%MAX_BROJ_KVADRATA_U_REDU)) + 1;
     broj_kvadratica += n;
-    niz_x = malloc(2*broj_kvadratica*sizeof(float));
-    niz_y = malloc(2*broj_kvadratica*sizeof(float));
+    niz_x = malloc(2*n*sizeof(float));
+    niz_y = malloc(2*n*sizeof(float));
+    postoji = malloc(n*sizeof(int));
+
+    niz_x[0] = x_1;
+    niz_x[1] = x_2;
+    niz_y[0] = y_1;
+    niz_y[1] = y_2;
+
+    int indikator_jedinice = 0;
+    for(int i=0; i<n; i++){
+        postoji[i] = abs((int)rand()%2);
+        if(postoji[i] == 1)
+            indikator_jedinice = 1;
+    }
+
+    if(indikator_jedinice == 0)
+        postoji[abs((int)rand()%n)] = 1;
 
     glutKeyboardFunc(on_keyboard);
     glutDisplayFunc(on_display);
@@ -205,7 +258,7 @@ int main(int argc, char** argv){
     v_x = 0.02;
     v_y = 0.02;
 
-    glClearColor(0,0,0,0);
+    glClearColor(0.75,0.75,0.75,0.75);
     glEnable(GL_DEPTH_TEST);
 
     glutMainLoop();
